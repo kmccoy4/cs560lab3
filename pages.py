1817c1316326f1a889c7905e6a8f2748ba04e17e
kmccoy4 <kmccoy4@vols.utk.edu>
2016-04-20 11:35:45 -0400

    preliminary link gathering

--- pages.py
+++ pages.py
@@ -7,7 +7,19 @@ def main():
         pos = line.find("<title>")
         if pos >= 0 and line.find(":") < 0:
             print '%s' % (line[pos+7:len(line)-9]);
+            #find associated text links
+            line = sys.stdin.next();
+            #skip lines not in text tags
+            while(line.find("<text") < 0):
+                line = sys.stdin.next();
 
+            while(line.find("</text>") < 0):
+                words = line.split()
+                for word in words:
+                    if word.find("[[") >= 0:
+                        print '%s%s' % (word,'\n');
+                line = sys.stdin.next();
+                
 
 if __name__ == "__main__":
     main()
