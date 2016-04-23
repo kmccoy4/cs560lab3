4da4007e19e2d3f89e46ff20a80f97e0df3e8940
kmccoy4 <kmccoy4@vols.utk.edu>
2016-04-22 22:03:00 -0400

    updated pages to print with 1

--- pages.py
+++ pages.py
@@ -3,6 +3,7 @@
 import sys, re, string
 
 def main():
+    re.compile('[a-zA-Z_0-9]')
     for line in sys.stdin:
         pos = line.find("<title>")
         if pos >= 0 and line.find(":") < 0:
@@ -14,46 +15,15 @@ def main():
                 line = sys.stdin.next();
 
             while(line.find("</text>") < 0):
-                #nline = line.replace('|','; ')
-                #nline = nline.replace(' ',';')
-                #words = re.split(r'[ |]',line)
-                words = line.split()
-                num = 0
-                for index,word in enumerate(words):
-                    wpos = word.find("[[")
-                    if wpos >= 0 and word.find(":") < 0 and word.find(".jpg") < 0:
-                        catstr = ""
-                        endpos = word.find("]]")
-                        if endpos > 0:
-                            if word.find("|") > 0:
-                                print '%s%s%s%d'% ('\t', word[wpos+2:word.find("|")],'\t',1)
-                            else:
-                                print '%s%s%s%d'% ('\t', word[wpos+2:endpos],'\t',1)
-                        else:
-                            catstr += word
-                            catstr += " "
-                        if endpos < 0:
-                            #get next word
-                            tempword = ""
-                            num = 1
-                            if index+1 < len(words):
-                                tempword = words[index+1]
-                            while(tempword.find("]]") < 0):
-                                catstr += tempword
-                                catstr += " "
-                                num+=1
-                                if index+num >= len(words):
-                                    break
-                                tempword = words[index+num]
-                            catstr += tempword
-                            if catstr.find("|") > 0:
-                                catstr = catstr[:catstr.find("|")]
-                                catstr += "]]"
-                            print '%s%s%s%d' % ('\t',catstr[catstr.find("[[")+2:catstr.find("]]")],'\t',1);
-                        #endpos = word.find("]]")
-#print '%s%s' % ('\t',word[wpos+2:endpos]);
+                links = re.findall(r"\[\[[\w|\|\s]*\]\]",line)
+                for l in links:
+                    if l.find("|") < 0:
+                        print "%s\t%d" % (l[2:len(l)-2],1)
+                    else:
+                        print "%s\t%d" % (l[2:l.find("|")],1)
                 line = sys.stdin.next();
-                
+
+
 
 if __name__ == "__main__":
     main()
