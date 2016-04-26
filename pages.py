9f69103cac38ebe797fffe3f91757ef36c44e987
kmccoy4 <kmccoy4@vols.utk.edu>
2016-04-26 01:04:33 -0400

    fixed regex

--- pages.py
+++ pages.py
@@ -2,7 +2,16 @@
 
 import sys, re, string
 
-re.compile('[a-zA-Z_0-9]')
+def xmlreplace(s):
+    s = s.replace("&quot;","\"")
+    s = s.replace("&lt;","<")
+    s = s.replace("&gt;",">")
+    s = s.replace("&apos;","\'")
+    s = s.replace("&amp;","&")        
+    return s
+
+re.compile('[a-zA-Z0-9]')
+
 pages = {}
 for line in sys.stdin:
     pos = line.find("<title>")
@@ -10,6 +19,7 @@ for line in sys.stdin:
         #print "%s%s" % ("Subject: ",line[pos+7:len(line)-9]);
         #add to dictionary
         subject = line[pos+7:len(line)-9]
+        subject = xmlreplace(subject)
         pages[subject]=[]
         #find associated text links
         line = sys.stdin.next();
@@ -18,14 +28,17 @@ for line in sys.stdin:
             line = sys.stdin.next();
 
         while(line.find("</text>") < 0):
-            links = re.findall(r"\[\[[\w|\|\s]*\]\]",line)
+            line = xmlreplace(line)
+            links = re.findall(r"(?<!'')\[\[[\w|\||\s|-|&|,|\.|!|\?|\(|\)|%|#|\*|;|\+|-|=|_|\^|\$|@|~|\"|\']*\]\]",line)
             for l in links:
-                if l.find("|") < 0:
-                    #print "%s\t%d" % (l[2:len(l)-2],1)
-                    pages[subject].append(l[2:len(l)-2])
-                else:
-                    #print "%s\t%d" % (l[2:l.find("|")],1)
-                    pages[subject].append(l[2:l.find("|")])
+                    if l.find("|") < 0:
+                        #print "%s\t%d" % (l[2:len(l)-2],1)
+                        pages[subject].append(l[2:len(l)-2])
+                    elif l.find("#") > 0:
+                        pages[subject].append(l[2:l.find("#")])
+                    else:
+                        #print "%s\t%d" % (l[2:l.find("|")],1)
+                        pages[subject].append(l[2:l.find("|")])
             line = sys.stdin.next();
 
 #print dictionary
