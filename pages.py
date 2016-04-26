4c90009a14a1caa278cc760c1e70750c53e3bb4f
kmccoy4 <kmccoy4@vols.utk.edu>
2016-04-26 12:10:14 -0400

    titles matched

--- pages.py
+++ pages.py
@@ -6,11 +6,29 @@ def xmlreplace(s):
     s = s.replace("&quot;","")
     s = s.replace("&lt;","<")
     s = s.replace("&gt;",">")
-    s = s.replace("&apos;","\'")
-    s = s.replace("&amp;","&")        
+    s = s.replace("&apos;"," ")
+    s = s.replace("&amp;","&")
+    s = s.replace("\'","_")
+    s = s.replace(" ","_")
     return s
 
 re.compile('[a-zA-Z0-9]')
+
+#get all titles
+if len(sys.argv) < 2:
+    print("usage: pages.py <titles_file>")
+    sys.exit(1)
+    
+f = open(sys.argv[1],"r")
+titles = f.read().splitlines()
+f.close()
+
+subjects = {}
+for title in titles:
+    title = title.lower()
+    title = xmlreplace(title)
+    if title not in subjects:
+        subjects[title] = []
 
 pages = {}
 for line in sys.stdin:
@@ -21,6 +39,8 @@ for line in sys.stdin:
             #add to dictionary
             subject = line[pos+7:len(line)-9]
             subject = xmlreplace(subject)
+            if subject not in subjects:
+                continue
             pages[subject]=[]
             #find associated text links
             line = sys.stdin.next();
@@ -31,30 +51,31 @@ for line in sys.stdin:
             while(line.find('</text>') < 0):
                 line = line.lower()
                 line = xmlreplace(line)
-                links = re.findall(r"(?<!'')\[\[[\w|\||\s|-|&|,|\.|!|\?|\(|\)|%|#|\*|;|\+|-|=|_|\^|\$|@|~|\']*\]\]",line)
+                links = re.findall(r"(?<!'')\[\[[\w|\||\s|-|&|,|\.|!|\?|\(|\)|%|#|\*|;|\+|-|=|_|\^|\$|@|~]*\]\]",line)
+
                 for l in links:
                         if l.find("|") < 0:
-                            #print "%s\t%d" % (l[2:len(l)-2],1)
-                            pages[subject].append(l[2:len(l)-2])
+                            l = l[2:len(l)-2]
                         elif l.find("#") > 0:
-                            pages[subject].append(l[2:l.find("#")])
+                            l = l[2:l.find("#")]
                         else:
-                            #print "%s\t%d" % (l[2:l.find("|")],1)
-                            pages[subject].append(l[2:l.find("|")])
+                            l = l[2:l.find("|")]
+                        if l in subjects:
+                            pages[subject].append(l)
                 line = sys.stdin.next();
 
 #print pages
 #remove links that do not exist as a subject (excess links, links that were not marked red)
-for sub in pages:
+#for sub in pages:
     #print "Subject: %s" % sub
     #print pages[sub]
-    for l in pages[sub]:
-        if l not in pages:
+#    for l in pages[sub]:
+#        if l not in pages:
             #print "removing %s" % l
-            pages[sub].remove(l)
+#            pages[sub].remove(l)
 
 #print dictionary
-#for sub in pages:
+for sub in pages:
     print "Subject: %s" % sub
     print pages[sub]
 #    for l in pages[sub]:
