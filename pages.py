c637e842c24e41c3cfd5cb42fc04a734b16f131c
kmccoy4 <kmccoy4@vols.utk.edu>
2016-04-26 10:43:53 -0400

    list issue with quotes

--- pages.py
+++ pages.py
@@ -3,7 +3,7 @@
 import sys, re, string
 
 def xmlreplace(s):
-    s = s.replace("&quot;","\"")
+    s = s.replace("&quot;","")
     s = s.replace("&lt;","<")
     s = s.replace("&gt;",">")
     s = s.replace("&apos;","\'")
@@ -14,35 +14,48 @@ re.compile('[a-zA-Z0-9]')
 
 pages = {}
 for line in sys.stdin:
-    pos = line.find("<title>")
-    if pos >= 0 and line.find(":") < 0:
-        #print "%s%s" % ("Subject: ",line[pos+7:len(line)-9]);
-        #add to dictionary
-        subject = line[pos+7:len(line)-9]
-        subject = xmlreplace(subject)
-        pages[subject]=[]
-        #find associated text links
-        line = sys.stdin.next();
-        #skip lines not in text tags
-        while(line.find("<text") < 0):
+        line = line.lower()
+        pos = line.find('<title>')
+        if pos >= 0 and line.find(":") < 0:
+            #print "%s%s" % ("Subject: ",line[pos+7:len(line)-9]);
+            #add to dictionary
+            subject = line[pos+7:len(line)-9]
+            subject = xmlreplace(subject)
+            pages[subject]=[]
+            #find associated text links
             line = sys.stdin.next();
+            #skip lines not in text tags
+            while(line.find('<text') < 0):
+                line = sys.stdin.next();
 
-        while(line.find("</text>") < 0):
-            line = xmlreplace(line)
-            links = re.findall(r"(?<!'')\[\[[\w|\||\s|-|&|,|\.|!|\?|\(|\)|%|#|\*|;|\+|-|=|_|\^|\$|@|~|\"|\']*\]\]",line)
-            for l in links:
-                    if l.find("|") < 0:
-                        #print "%s\t%d" % (l[2:len(l)-2],1)
-                        pages[subject].append(l[2:len(l)-2])
-                    elif l.find("#") > 0:
-                        pages[subject].append(l[2:l.find("#")])
-                    else:
-                        #print "%s\t%d" % (l[2:l.find("|")],1)
-                        pages[subject].append(l[2:l.find("|")])
-            line = sys.stdin.next();
+            while(line.find('</text>') < 0):
+                line = line.lower()
+                line = xmlreplace(line)
+                links = re.findall(r"(?<!'')\[\[[\w|\||\s|-|&|,|\.|!|\?|\(|\)|%|#|\*|;|\+|-|=|_|\^|\$|@|~|\']*\]\]",line)
+                for l in links:
+                        if l.find("|") < 0:
+                            #print "%s\t%d" % (l[2:len(l)-2],1)
+                            pages[subject].append(l[2:len(l)-2])
+                        elif l.find("#") > 0:
+                            pages[subject].append(l[2:l.find("#")])
+                        else:
+                            #print "%s\t%d" % (l[2:l.find("|")],1)
+                            pages[subject].append(l[2:l.find("|")])
+                line = sys.stdin.next();
+
+#print pages
+#remove links that do not exist as a subject (excess links, links that were not marked red)
+for sub in pages:
+    #print "Subject: %s" % sub
+    #print pages[sub]
+    for l in pages[sub]:
+        if l not in pages:
+            #print "removing %s" % l
+            pages[sub].remove(l)
 
 #print dictionary
-for sub in pages:
+#for sub in pages:
     print "Subject: %s" % sub
-    for l in pages[sub]:
-        print l
+    print pages[sub]
+#    for l in pages[sub]:
+#        print l
